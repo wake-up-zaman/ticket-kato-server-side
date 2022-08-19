ec111542a8408e7bb17a33d0f75dbe73c47c3793
wakeupzaman909 <hadiuzzaman909@outlook.com>
2022-08-19 15:51:22 +0600

    test

--- routes/buses.js
+++ routes/buses.js
@@ -2,7 +2,7 @@ import express from "express"
 import { createBuses, deleteBuses, getAllBuses, getBuses, updateBuses } from "../controllers/controller_buses.js";
 const router=express.Router();
 
-//CREATE
+//CREATE of buses
 router.post("/", createBuses);
 
 //UPDATE
