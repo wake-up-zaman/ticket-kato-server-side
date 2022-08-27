7aac88accb6a34c4bcc440ae71f877f7a8f3e5d6
wakeupzaman909 <hadiuzzaman909@outlook.com>
2022-08-27 23:47:22 +0600

    vercel file added

--- models/model_buses.js
+++ models/model_buses.js
@@ -9,7 +9,7 @@ const BusesSchema=new mongoose.Schema({
     },
     operator_name:{
         type:String,
-        required:true
+
     },
     operator_names:{
         type:[String],
@@ -19,35 +19,35 @@ const BusesSchema=new mongoose.Schema({
     },
     bus_type:{
         type:String,
-        required:true
+
     },
     bus_class:{
         type:String,
-        required:true
+
     },
     coach_number:{
         type: Number,
-        required:true
+
     },
     route:{
         type:String,
-        required:true
+
     },
     departure_city:{
         type:String,
-        required:true        
+   
     },
     arrival_city:{
         type:String,
-        required:true        
+     
     },
     departure_time:{
         type:String,
-        required:true        
+      
     },
     arrival_time:{
         type:String,
-        required:true        
+      
     },
     photos:{
         type:[String],
@@ -57,15 +57,15 @@ const BusesSchema=new mongoose.Schema({
     },
     boarding_point:{
         type:String,
-        required:true
+
     },
     dropping_point:{
         type:String,
-        required:true
+
     },
     price:{
         type: Number,
-        required:true
+
     },
     rating:{
         type:Number,
